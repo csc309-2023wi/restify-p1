f1c226408788d8c0fffaa08f06a3b18049a55672
Samm Du <hmu@sammdu.com>
2023-02-11 20:56:35 -0500

    use splide image carousel

--- js/templates.js
+++ js/templates.js
@@ -4,6 +4,7 @@ const templateChildrenUrls = {
     footerContent: "templates/footer.html",
     propertyCardGeneric: "templates/card_property_listing_generic.html",
     modalReference: "templates/modal_ref.html",
+    modalUserUnbooked: "templates/modal_user_unbooked.html",
     modalHostCreate: "templates/modal_host_create.html",
 };
 
@@ -12,8 +13,10 @@ function renderTemplates() {
     // grab the template container
     const bodyTemplate = document.getElementById("mustache").innerHTML;
     const templateChildren = {};
-    Object.keys(templateChildrenUrls).forEach((templateId) => {
-        fetch(templateChildrenUrls[templateId])
+
+    // dispatch tasks to render templates
+    const renderTasks = Object.keys(templateChildrenUrls).map((templateId) => {
+        return fetch(templateChildrenUrls[templateId])
             .then((response) => response.text())
             .then((child) => {
                 templateChildren[templateId] = child;
@@ -25,5 +28,11 @@ function renderTemplates() {
                 // insert the rendered template into the body tag
                 document.getElementById("body").innerHTML = rendered;
             });
+    });
+
+    // load the image carousel after templates have been rendered
+    Promise.all(renderTasks).then(() => {
+        var splide = new Splide(".splide");
+        splide.mount();
     });
 }
