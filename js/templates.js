3e98abe69f3a29faa538d4843a4cca48414f85d5
Samm Du <samm.du@mail.utoronto.ca>
2023-02-12 23:51:57 -0500

    User Modals (#6)
    * response design working so far
    
    * Completed draft for profile and dashboard page
    
    * fixed responsiveness of profile.html and changed components.css so navbar popups work properly
    
    * revert miscellaneous
    
    * simplified profile design; refactored code
    
    * use icon images rather than inline svg
    
    * simplify dashboard layout code + use icon images
    
    * dashboard make section titles more responsive
    
    * Add host modal draft
    
    * modal common initially complete
    
    * added close button
    
    * use splide image carousel
    
    * remove junk; fixed messed up padding
    
    * fixed responsiveness issues
    
    * added action widget
    
    * completed action widget
    
    * added prices; improved comments
    
    * host card complete
    
    * added hyperlink to host's email
    
    * added booked modal
    
    * make sure action bar width fits host card
    
    * Add user modal information
    
    * fixed z-indices
    
    * one more z-index
    
    * added modals
    
    * fixed logo color
    
    ---------
    
    Co-authored-by: aryanbagga <aryanbagga8@gmail.com>
    Co-authored-by: Shawn Plotko <shawn.plotko@mail.utoronto.ca>

--- js/templates.js
+++ js/templates.js
@@ -3,6 +3,8 @@ const templateChildrenUrls = {
     navContent: "templates/navbar.html",
     footerContent: "templates/footer.html",
     propertyCardGeneric: "templates/card_property_listing_generic.html",
+    modalUserUnbooked: "templates/modal_user_unbooked.html",
+    modalUserBooked: "templates/modal_user_booked.html",
     modalHostCreate: "templates/modal_host_create.html",
     modalHostExisting: "templates/modal_host_existing.html",
 };
@@ -12,8 +14,10 @@ function renderTemplates() {
     // grab the template container
     const bodyTemplate = document.getElementById("mustache").innerHTML;
     const templateChildren = {};
-    Object.keys(templateChildrenUrls).forEach((templateId) => {
-        fetch(templateChildrenUrls[templateId])
+
+    // dispatch tasks to render templates
+    const renderTasks = Object.keys(templateChildrenUrls).map((templateId) => {
+        return fetch(templateChildrenUrls[templateId])
             .then((response) => response.text())
             .then((child) => {
                 templateChildren[templateId] = child;
@@ -25,5 +29,11 @@ function renderTemplates() {
                 // insert the rendered template into the body tag
                 document.getElementById("body").innerHTML = rendered;
             });
+    });
+
+    // load the image carousel after templates have been rendered
+    Promise.all(renderTasks).then(() => {
+        var splide = new Splide(".splide");
+        splide.mount();
     });
 }
